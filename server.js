af02d650d3f488169b80756d6d7d3746f2c81b86
Jayesh087 <jayeshparmar635453@gmail.com>
2025-07-16 11:04:02 +0530

    feat: add sweet function test case pass

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import express from 'express';
+const express = require('express');
 const app = express();
+
 app.use(express.json());
 
-app.post('/api/sweets', (req, res) => {
+app.post('/api/v1/sweets/add', (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(400).json({ message: 'Name required' });
   res.status(201).json({ message: 'Sweet added' });
 });
 
-export default app;
-
-// Run only if not testing
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(5000, () => console.log('Server running'));
+  app.listen(5000, () => console.log('Server started on 5000'));
 }
+
+module.exports = app;
