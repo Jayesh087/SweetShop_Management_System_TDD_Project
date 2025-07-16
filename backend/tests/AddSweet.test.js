8de146a16fad5d667174b58ce4ca4a91b718be6a
Jayesh087 <jayeshparmar635453@gmail.com>
2025-07-16 11:02:00 +0530

    test: add sweet function availability failing test

--- backend/tests/AddSweet.test.js
+++ backend/tests/AddSweet.test.js
@@ -1,12 +1,9 @@
-import request from 'supertest';
-import app from '../server.js';
+const request = require('supertest');
+const app = require('../../server');
 
 describe('Add Sweet API', () => {
-  test('should return 400 if name is missing', async () => {
-    const res = await request(app).post('/api/sweets').send({ price: 10 });
+  test('should fail when name is missing', async () => {
+    const res = await request(app).post('/api/v1/sweets/add').send({ price: 10 });
     expect(res.statusCode).toBe(400);
   });
 });
-
-
-
